9a34d508ccf14212d41d681afa52ee0a190de7cd
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2017-10-22 22:23:38 -0200

    few changes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 console.log('Starting app.js')
 
 const fs = require('fs')
-const os = require('os')
+const _ = require('lodash')
+
 const notes = require('./notes.js')
 
-let user = os.userInfo()
+let command = process.argv[2];
+console.log(process.argv)
+console.log('Command:', command)
 
-fs.appendFile('name.txt', `Hello, ${user.username}`)
+if (command === 'add') {
+  console.log('adding new note')
+} else if (command === 'list') {
+  console.log('listing notes')
+} else if (command === 'read') {
+  console.log('reading notes')
+} else if (command === 'remove') {
+  console.log('removing note')
+} else {
+  console.log('invalid command')
+}
--- notes.js
+++ notes.js
@@ -1,6 +1,6 @@
 console.log('starting notes.js')
 
 module.exports.addNote = () => {
-    console.log('adding note')
-    return 'new note'
+  console.log('adding note')
+  return 'new note'
 }
